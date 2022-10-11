0cde2153ab06ee6f3fa31914921d1854fb12dc7c
Evandro Martinelli <martinelli.evandro@gmail.com>
2022-10-11 12:48:05 -0300

    (test) add test

--- internal/core/order/place_order_use_case_test.go
+++ internal/core/order/place_order_use_case_test.go
@@ -1,1 +1,71 @@
 package order_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+
+	"github.com/evmartinelli/go-rifa-microservice/internal/adapters/idgenerator"
+	mock_raffle "github.com/evmartinelli/go-rifa-microservice/internal/core/mock"
+	"github.com/evmartinelli/go-rifa-microservice/internal/core/order"
+	"github.com/evmartinelli/go-rifa-microservice/internal/core/raffle"
+	"github.com/evmartinelli/go-rifa-microservice/pkg/assert"
+)
+
+type test struct {
+	name string
+	mock func()
+	err  error
+}
+
+var errReachedLimit = errors.New("user reached the limit")
+
+func placeOrderUseCase(t *testing.T) (uc *order.PlaceOrderUseCase, or *MockRepo, rr *mock_raffle.MockRepo, p *MockPixPayment) {
+	t.Helper()
+
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+
+	orderRepo := NewMockRepo(mockCtl)
+	raffleRepo := mock_raffle.NewMockRepo(mockCtl)
+	uuid := idgenerator.NewUUIDGenerator()
+	pix := NewMockPixPayment(mockCtl)
+
+	placeOrderUseCase := order.NewPlaceOrderUseCase(orderRepo, raffleRepo, pix, uuid)
+
+	return placeOrderUseCase, orderRepo, raffleRepo, pix
+}
+
+func TestCreateOrder(t *testing.T) {
+	t.Parallel()
+
+	orderUseCase, repo, raffleRepo, pix := placeOrderUseCase(t)
+
+	tests := []test{
+		{
+			name: "User Reached Limit",
+			mock: func() {
+				repo.EXPECT().CreateOrder(context.Background(), order.Order{}).Return(order.Order{}, nil)
+				repo.EXPECT().GetUserOrders(gomock.Any(), gomock.Any()).Return([]order.Order{}, nil)
+				raffleRepo.EXPECT().GetProduct(context.Background(), gomock.Any()).Return(raffle.Raffle{UserLimit: 1}, nil)
+				pix.EXPECT().GeneratePix().Return(order.Pix{}, nil)
+			},
+			err: errReachedLimit,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			tc.mock()
+			res, err := orderUseCase.Run(context.Background(), &order.Request{})
+			assert.NotNil(t, err)
+			assert.NotNil(t, res)
+		})
+	}
+}
