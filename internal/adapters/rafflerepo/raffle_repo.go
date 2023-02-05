7138423f3d2ff8b4173eb84dfbd0c8a7cc764f6e
Evandro Martinelli <martinelli.evandro@gmail.com>
2023-02-05 19:15:18 -0300

    fix repo

--- internal/adapters/rafflerepo/raffle_repo.go
+++ internal/adapters/rafflerepo/raffle_repo.go
@@ -3,14 +3,15 @@ package rafflerepo
 import (
 	"github.com/evmartinelli/go-rifa-microservice/internal/adapters/rafflerepo/dynamodb"
 	"github.com/evmartinelli/go-rifa-microservice/internal/adapters/rafflerepo/postgres"
+	repo "github.com/evmartinelli/go-rifa-microservice/internal/core/raffle"
 	db "github.com/evmartinelli/go-rifa-microservice/pkg/dynamodb"
 	pg "github.com/evmartinelli/go-rifa-microservice/pkg/postgres"
 )
 
-func NewDynamoDBRaffleRepo(config *db.DynamoConfig) *dynamodb.RaffleRepo {
+func NewDynamoDBRaffleRepo(config *db.DynamoConfig) repo.Repo {
 	return dynamodb.NewRaffleRepo(config)
 }
 
-func NewPostgresRaffleRepo(config *pg.Database) *postgres.RaffleRepo {
+func NewPostgresRaffleRepo(config *pg.Database) repo.Repo {
 	return postgres.NewRaffleRepo(config)
 }
